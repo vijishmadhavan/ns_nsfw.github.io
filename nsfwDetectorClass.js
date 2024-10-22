5f348fae086cc6be557cb4a1f25a8caf33faed9f
Vijish Madhavan <53169213+vijishmadhavan@users.noreply.github.com>
2024-10-22 22:37:44 +0530

    Update nsfwDetectorClass.js

--- nsfwDetectorClass.js
+++ nsfwDetectorClass.js
@@ -1,7 +1,7 @@
 class NsfwDetector {
     constructor() {
         this.model = null;
-        this.MODEL_URL = './models/inception_v3/model.json';
+        this.MODEL_URL = './models/mobilenet_v2/model.json';
         this.classes = ['drawing', 'hentai', 'neutral', 'porn', 'sexy'];
     }
 
@@ -17,7 +17,7 @@ class NsfwDetector {
     async classifyImage(imageElement) {
         const model = await this.loadModel();
         const tensor = tf.browser.fromPixels(imageElement)
-            .resizeBilinear([299, 299])
+            .resizeBilinear([224, 224]) // MobileNet V2 expects 224x224 images
             .toFloat()
             .div(tf.scalar(255))
             .expandDims();
@@ -45,7 +45,7 @@ class NsfwDetector {
         results.sort((a, b) => b.probability - a.probability);
 
         // Determine if the image is NSFW
-        const isNSFW = predictions[1] > 0.2 || predictions[3] > 0.2 || predictions[4] > 0.2;
+        const isNSFW = predictions[1] > 0.1 || predictions[3] > 0.1 || predictions[4] > 0.1;
 
         const result = {
             isNSFW,
