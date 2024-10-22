7c9bcd0fb5c1674d76a5c84e52b683ac013ebcf8
Vijish Madhavan <53169213+vijishmadhavan@users.noreply.github.com>
2024-10-22 13:12:30 +0530

    Add files via upload

---
+++ combinedDetector.js
@@ -0,0 +1,54 @@
+class CombinedDetector {
+    constructor() {
+        this.nsfwDetector = new window.NsfwDetector();
+        this.ageThreshold = 22;
+    }
+
+    async initialize() {
+        await Promise.all([
+            this.nsfwDetector.loadModel(),
+            faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+            faceapi.nets.ageGenderNet.loadFromUri('/models')
+        ]);
+    }
+
+    async analyzeImage(imageUrl) {
+        const img = await this.loadImage(imageUrl);
+        const [nsfwResult, ageResult] = await Promise.all([
+            this.nsfwDetector.isNsfw(imageUrl),
+            this.detectAge(img)
+        ]);
+
+        const isNSFW = nsfwResult.isNSFW || (ageResult !== null && ageResult < this.ageThreshold);
+
+        return {
+            isNSFW,
+            age: ageResult,
+            nsfwResults: nsfwResult.results
+        };
+    }
+
+    async loadImage(url) {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.crossOrigin = 'anonymous';
+            img.onload = () => resolve(img);
+            img.onerror = reject;
+            img.src = url;
+        });
+    }
+
+    async detectAge(img) {
+        const detections = await faceapi
+            .detectAllFaces(img, new faceapi.TinyFaceDetectorOptions())
+            .withAgeAndGender();
+
+        if (detections.length > 0) {
+            return Math.round(detections[0].age);
+        }
+        return null;
+    }
+}
+
+window.CombinedDetector = CombinedDetector;
+
---
+++ nsfwDetectorClass.js
@@ -0,0 +1,59 @@
+class NsfwDetector {
+    constructor() {
+        this.model = null;
+        this.MODEL_URL = './models/inception_v3/model.json';
+        this.classes = ['drawing', 'hentai', 'neutral', 'porn', 'sexy'];
+    }
+
+    async loadModel() {
+        if (!this.model) {
+            console.log('Loading model from:', this.MODEL_URL);
+            this.model = await tf.loadLayersModel(this.MODEL_URL);
+            console.log('Model loaded:', this.model);
+        }
+        return this.model;
+    }
+
+    async classifyImage(imageElement) {
+        const model = await this.loadModel();
+        const tensor = tf.browser.fromPixels(imageElement)
+            .resizeBilinear([299, 299])
+            .toFloat()
+            .div(tf.scalar(255))
+            .expandDims();
+        const predictions = await model.predict(tensor);
+        return predictions.dataSync();
+    }
+
+    async isNsfw(imageUrl) {
+        const img = new Image();
+        img.crossOrigin = 'anonymous';
+        img.src = imageUrl;
+
+        await new Promise((resolve, reject) => {
+            img.onload = resolve;
+            img.onerror = reject;
+        });
+
+        const predictions = await this.classifyImage(img);
+        const results = this.classes.map((className, index) => ({
+            className,
+            probability: predictions[index]
+        }));
+
+        // Sort results by probability in descending order
+        results.sort((a, b) => b.probability - a.probability);
+
+        // Determine if the image is NSFW
+        const isNSFW = predictions[1] > 0.2 || predictions[3] > 0.2 || predictions[4] > 0.2;
+
+        const result = {
+            isNSFW,
+            results
+        };
+        console.log('NSFW detection result:', result);
+        return result;
+    }
+}
+
+window.NsfwDetector = NsfwDetector;
